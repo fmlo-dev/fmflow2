f68429c22e1efafd99c4ab3eba973b1b54a95008
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-09-04 13:38:44 -0300

    Update: __init__.py
    + add useful funtions (open ,getheader, getdata)

--- fmflowc/io/fits/__init__.py
+++ fmflowc/io/fits/__init__.py
@@ -4,5 +4,20 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# dependent libraries
+from astropy.io import fits
+
 # sub modules/functions
 from .aste.merge_data import fromaste
+
+
+def open(*args, **kwargs):
+    return fits.open(*args, **kwargs)
+
+
+def getheader(*args, **kwargs):
+    return fits.getheader(*args, **kwargs)
+
+
+def getdata(*args, **kwargs):
+    return fits.getdata(*args, **kwargs)
