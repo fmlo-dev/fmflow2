f443c778450e6b6d4cd31a1a519eacb899e8ce85
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-10-16 18:37:29 +0900

    Delete: makearray.py

--- fmflowc/io/fits/__init__.py
+++ fmflowc/io/fits/__init__.py
@@ -4,9 +4,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# dependent libraries
-from astropy.io.fits import *
-
 # sub modules/functions
-from ._aste.mergedata import astemerge
-from ._aste.makearray import astearray
+from ._aste.mergedata import fromaste
