ec5be4ba753d92d6bcb6d6730f8849197718c95f
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-10-16 23:53:10 +0900

    Update: import * of numpy in fmflowc.ana.array

--- fmflowc/ana/array/__init__.py
+++ fmflowc/ana/array/__init__.py
@@ -4,6 +4,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# standard libraries
+from numpy import *
+
 # sub modules/functions
 from .fmarray import FMArray
 from .fdarray import FDArray
