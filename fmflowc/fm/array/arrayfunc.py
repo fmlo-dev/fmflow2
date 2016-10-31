1166e2608e5f9e39c9366dfb30748f313b6f0006
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-10-31 13:55:25 +0900

    Add: fmfunc and timechunk decorators

--- fmflowc/fm/array/arrayfunc.py
+++ fmflowc/fm/array/arrayfunc.py
@@ -4,10 +4,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# standard libraries
+from functools import partial, wraps
+from inspect import getargspec
+
 # dependent libraries
 import numpy as np
 import numpy.ma as ma
 from fmflowc.utils import exceptions as e
+from fmflowc.utils import multiprocessing as mp
 
 # sub modules/functions
 from .array import FMData, FMArray
@@ -57,3 +62,39 @@ def concatenate(arrays):
     info.update(array_1.info)
     
     return FMArray.frommaskedarray(array, table, info)
+
+def fmfunc(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        fmarray_in  = kwargs.pop('array_in', args[0])
+        fmarray_out = array_in.copy()
+
+        array_in = np.asarray(fmarray_in)
+        array_out = func(array_in, *args[1:], **kwargs)
+
+        fmarray_out.data[:] = array_out
+        return fmarray_out
+
+    return wrapper
+
+def timechunk(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        argnames = getargspec(func).args
+        for i in range(len(args)):
+            kwargs[argnames[i]] = args[i]
+
+        array_in  = kwargs.pop('array_in')
+        chunk_len = kwargs.pop('chunk_len', len(array_in))
+        chunk_num = round(len(array_in)/chunk_len)
+
+        index = np.linspace(0, len(array_in), chunk_num+1, dtype=int)
+        subs_in = [array_in[index[i]:index[i+1]] for i in range(len(index)-1)]
+
+        p = mp.Pool()
+        pfunc = partial(func, **kwargs)
+        subs_out = p.map(pfunc, subs_in)
+        array_out = np.concatenate(subs_out)
+        return array_out
+
+    return wrapper
