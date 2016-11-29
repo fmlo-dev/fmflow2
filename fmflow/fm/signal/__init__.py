da98ae40af79883ceedcdc35480ff3203d026ca6
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2016-11-29 09:32:39 +0900

    Add statistics module

--- fmflow/fm/signal/__init__.py
+++ fmflow/fm/signal/__init__.py
@@ -7,8 +7,10 @@ from __future__ import print_function as _print_function
 # submodules
 from .decomposition import *
 from .filtering import *
+from .statistics import *
 
 # imported items
 __all__ = []
 __all__ += decomposition.__all__
 __all__ += filtering.__all__
+__all__ += statistics.__all__
---
+++ fmflow/fm/signal/statistics.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from __future__ import absolute_import as _absolute_import
+from __future__ import division as _division
+from __future__ import print_function as _print_function
+
+# dependent packages
+import numpy as np
+import numpy.ma as ma
+
+# imported items
+__all__ = ['mad']
+
+
+def mad(array, axis=None):
+    """Compute the median absolute deviation (MAD) along the given axis.
+
+    Args
+    ----
+
+    - array (array-like): An input array.
+    - axis (int, None): Axis along which the MADs are computed.
+      The default is to compute the MAD along a flattened version of the array.
+    
+    Returns
+    -------
+
+    - mad (array): 
+    """
+    if axis == 1:
+        mad = ma.median(ma.abs(array - ma.median(array, 1)[:,np.newaxis]), 1)
+    else:
+        mad = ma.median(ma.abs(array - ma.median(array, axis)), axis)
+    
+    return mad
