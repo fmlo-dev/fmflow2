15c1274c07763b1949ceaeb8d79293e97056ec88
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-01-22 16:01:28 +0900

    Delete logo

--- fmflow/__init__.py
+++ fmflow/__init__.py
@@ -1,13 +1,5 @@
 # coding: utf-8
 
-"""
- _____ __  __ _____ _               
-|  ___|  \/  |  ___| | _____      __
-| |_  | |\/| | |_  | |/ _ \ \ /\ / /
-|  _| | |  | |  _| | | (_) \ V  V / 
-|_|   |_|  |_|_|   |_|\___/ \_/\_/  
-"""
-
 # Python 3.x compatibility
 from __future__ import absolute_import as __absolute_import
 from __future__ import division as __division
