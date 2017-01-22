0f251ad1ece770cf8535acf30ea0900e497294b7
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-01-22 16:15:37 +0900

    Update build.sh

--- docs/_project/build.sh
+++ docs/_project/build.sh
@@ -1,2 +1,9 @@
-sphinx-apidoc -f -o ./apis ../../fmflow
-sphinx-build . ../
+#!/bin/bash
+
+if ! type sphinx-build >/dev/null 2>&1; then
+    echo "sphinx is not installed"
+    exit 1
+fi
+
+#sphinx-apidoc -f -o ./apis ../../fmflow
+sphinx-build -a -d ../_doctree ./ ../
