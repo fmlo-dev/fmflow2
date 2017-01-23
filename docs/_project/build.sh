0337f9f987bd96e3829b28ac0ba21375b4b3b904
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-01-23 16:30:19 +0900

    Add a prompt for confirmation before build

--- docs/_project/build.sh
+++ docs/_project/build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-if ! type sphinx-build >/dev/null 2>&1; then
-    echo "sphinx is not installed"
-    exit 1
-fi
+prompt () {
+    echo -ne "${1} (y/[n])" 
+    read answer
+    case ${answer} in
+        [yY]* ) return 0;;
+        *     ) return 1;;
+    esac
+}
 
-#sphinx-apidoc -f -o ./apis ../../fmflow
-sphinx-build -a -d ../_doctree ./ ../
-sphinx-build -a -d ../_doctree ./ ../
+echo "WARN: this will (re)write all files of fmflow/docs"
+echo "we recommend to use testbuild.sh to build fmflow/testdocs"
+echo "(this will not change any files of fmflow/docs)"
+echo ""
+
+if prompt "continue to build?"; then
+    if type sphinx-build >/dev/null 2>&1; then
+        #sphinx-apidoc -f -o ./apis ../../fmflow
+        sphinx-build -a -d ../_doctree ./ ../
+        sphinx-build -a -d ../_doctree ./ ../
+    else
+        echo "ERROR: sphinx is not installed"
+        exit 1
+    fi
+fi
