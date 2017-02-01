61116381c5b0be7061fcd2aff4c1f1793c861927
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-02-01 17:16:50 +0900

    Minor code revision

--- examples/fmfits_aste_mac.py
+++ examples/fmfits_aste_mac.py
@@ -8,9 +8,10 @@ FMFlow version: 0.1
 
 from fmflow.io import fits
 
-fmlolog    = 'FMLOLOG.yyyymmddhhmmss'
-antennalog = 'ANTENNALOG.yyyymmddhhmmss'
-backendlog = 'ACG.<obstable>.<user>.<proj>.yyyymmddhhmmss.s'
+fmlolog    = 'FMLOLOG.yyyymmddhhmmss' # replace it with yours
+antennalog = 'ANTENNALOG.yyyymmddhhmmss' # replace it with yours
+backendlog = 'ACG.obstable.user.proj.yyyymmddhhmmss.s' # replace it with yours
+fitsname   = 'saved_fmfits.fits' # replace it with yours
 
 f = fits.fromaste(fmlolog, backendlog, antennalog)
-f.writeto('example.fits')
+f.writeto(fitsname)
