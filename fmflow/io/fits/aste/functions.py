31e5e378ddb5f38744b5c96970d75bd1751b9b48
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-02-14 22:22:17 +0900

    Update merge functions
    compatible with fm.utils.CStructReader

--- fmflow/io/fits/aste/functions.py
+++ fmflow/io/fits/aste/functions.py
@@ -12,14 +12,13 @@ from __future__ import print_function as _print_function
 # the Python standard library
 import re
 import json
-from collections import OrderedDict
 
 # the Python Package Index
 import numpy as np
+import yaml
+import fmflow as fm
 from astropy.io import fits
 from astropy.coordinates import Angle
-from fmflow import __version__
-from fmflow import utils as ut
 
 # imported items
 __all__ = ['fromaste']
@@ -29,13 +28,14 @@ LAT_ASTE   = Angle('-22d58m17.69447s').deg
 EFF_8257D  = 0.92 # exposure / interval time of Agilent 8257D
 
 
-def fromaste(fmlolog, backendlog, antennalog=None):
+def fromaste(fmlolog, backendlog, antennalog=None, byteorder='<'):
     """Read logging data of ASTE and merge them into a FITS object.
 
     Args:
         fmlolog (str): File name of FMLO logging.
         backendlog (str): File name of backend logging.
         antennalog (str): File name of antenna logging (optional).
+        byteorder (str):
 
     Returns:
         fitsobj (HDUlist): HDU list containing the merged data.
@@ -53,15 +53,15 @@ def fromaste(fmlolog, backendlog, antennalog=None):
     fitsobj.append(hdu)
 
     # BACKEND HDU
-    backend = _check_backend(backendlog)
+    backend = _check_backend(backendlog, byteorder)
 
     if backend == 'AC45':
-        hdu = _read_backendlog_mac(backendlog)
+        hdu = _read_backendlog_mac(backendlog, byteorder)
     elif backend == 'FFX':
-        # hdu = _read_backendlog_whsf(backendlog)
-        raise ut.FMFlowError('WHSF logging is not supported yet')
+        # hdu = _read_backendlog_whsf(backendlog, byteorder)
+        raise fm.utils.FMFlowError('WHSF logging is not supported yet')
     else:
-        raise ut.FMFlowError('invalid logging type')
+        raise fm.utils..FMFlowError('invalid logging type')
 
     fitsobj.append(hdu)
 
@@ -88,8 +88,7 @@ def _read_fmlolog(fmlolog):
 
     """
     # datetime converter
-    c = ut.DatetimeConverter('%Y%m%d%H%M%S.%f')
-
+    c = fm.utils.DatetimeConverter('%Y%m%d%H%M%S.%f')
     starttime = np.loadtxt(fmlolog, str, usecols=(0,), skiprows=1, converters={0: c})
     scantype  = np.loadtxt(fmlolog, str, usecols=(1,), skiprows=1)
     fmfreq    = np.loadtxt(fmlolog, float, usecols=(2,), skiprows=1)
@@ -107,10 +106,10 @@ def _read_fmlolog(fmlolog):
     columns = []
     columns.append(fits.Column('STARTTIME', 'A26', 'ISO 8601', array=starttime))
     columns.append(fits.Column('SCANTYPE',  'A4', '', array=scantype))
-    columns.append(fits.Column('FMFLAG',    'L', 'bool', array=fmflag))
-    columns.append(fits.Column('FMFREQ',    'D', 'Hz', array=fmfreq))
-    columns.append(fits.Column('LOFREQ',    'D', 'Hz', array=lofreq))
-    columns.append(fits.Column('VRAD',      'D', 'm/s', array=vrad))
+    columns.append(fits.Column('FMFLAG', 'L', 'bool', array=fmflag))
+    columns.append(fits.Column('FMFREQ', 'D', 'Hz', array=fmfreq))
+    columns.append(fits.Column('LOFREQ', 'D', 'Hz', array=lofreq))
+    columns.append(fits.Column('VRAD', 'D', 'm/s', array=vrad))
 
     hdu = fits.BinTableHDU.from_columns(columns, header)
     return hdu
@@ -127,8 +126,7 @@ def _read_antennalog(antennalog):
 
     """
     # datetime converter
-    c = ut.DatetimeConverter('%y%m%d%H%M%S.%f')
-
+    c = fm.utils.DatetimeConverter('%y%m%d%H%M%S.%f')
     starttime = np.loadtxt(antennalog, str, usecols=(0,), skiprows=1, converters={0: c})
     ra_prog   = np.loadtxt(antennalog, float, usecols=(1,), skiprows=1)
     dec_prog  = np.loadtxt(antennalog, float, usecols=(2,), skiprows=1)
@@ -156,39 +154,43 @@ def _read_antennalog(antennalog):
 
     columns = []
     columns.append(fits.Column('STARTTIME', 'A26', 'ISO 8601', array=starttime))
-    columns.append(fits.Column('RA',        'D', 'deg', array=ra_real))
-    columns.append(fits.Column('DEC',       'D', 'deg', array=dec_real))
-    columns.append(fits.Column('AZ',        'D', 'deg', array=az_real))
-    columns.append(fits.Column('EL',        'D', 'deg', array=el_real))
-    columns.append(fits.Column('RA_PROG',   'D', 'deg', array=ra_prog))
-    columns.append(fits.Column('DEC_PROG',  'D', 'deg', array=dec_prog))
-    columns.append(fits.Column('AZ_PROG',   'D', 'deg', array=az_prog))
-    columns.append(fits.Column('EL_PROG',   'D', 'deg', array=el_prog))
-    columns.append(fits.Column('RA_ERROR',  'D', 'deg', array=ra_error))
+    columns.append(fits.Column('RA', 'D', 'deg', array=ra_real))
+    columns.append(fits.Column('DEC', 'D', 'deg', array=dec_real))
+    columns.append(fits.Column('AZ', 'D', 'deg', array=az_real))
+    columns.append(fits.Column('EL', 'D', 'deg', array=el_real))
+    columns.append(fits.Column('RA_PROG', 'D', 'deg', array=ra_prog))
+    columns.append(fits.Column('DEC_PROG', 'D', 'deg', array=dec_prog))
+    columns.append(fits.Column('AZ_PROG', 'D', 'deg', array=az_prog))
+    columns.append(fits.Column('EL_PROG', 'D', 'deg', array=el_prog))
+    columns.append(fits.Column('RA_ERROR', 'D', 'deg', array=ra_error))
     columns.append(fits.Column('DEC_ERROR', 'D', 'deg', array=dec_error))
-    columns.append(fits.Column('AZ_ERROR',  'D', 'deg', array=az_error))
-    columns.append(fits.Column('EL_ERROR',  'D', 'deg', array=el_error))
+    columns.append(fits.Column('AZ_ERROR', 'D', 'deg', array=az_error))
+    columns.append(fits.Column('EL_ERROR', 'D', 'deg', array=el_error))
 
     hdu = fits.BinTableHDU.from_columns(columns, header)
     return hdu
 
 
-def _check_backend(backendlog):
+def _check_backend(backendlog, byteorder):
     """Check backend type from a backend logging of ASTE.
 
     Args:
         backendlog (str): File name of backend logging.
+        byteorder (str):
 
     Returns:
         backend (str): Backend type.
 
     """
-    from .otflog_common import HEAD, CTL
+    with open('struct_common.yaml') as f:
+         d = yaml.load(f)
+         head = fm.utils.CStructReader(d['head'])
+         ctl  = fm.utils.CStructReader(d['ctl'])
 
     with open(backendlog, 'rb') as f:
-        ut.readbinary(f, HEAD)
-        readdata = ut.readbinary(f, CTL)
-        backend = readdata['cbe_type']
+        head.read(f)
+        ctl.read(f)
+        backend = ctl.data['cbe_type']
 
     return backend
 
@@ -198,60 +200,59 @@ def _read_backendlog_mac(backendlog):
 
     Args:
         backendlog (str): File name of backend logging.
+        byteorder (str):
 
     Returns:
         hdu (BinTableHDU): HDU containing the read backend logging.
 
     """
-    from .otflog_common import HEAD, CTL
-    from .otflog_mac import OBS, DAT
+    with open('struct_common.yaml') as f:
+         d = yaml.load(f)
+         head = fm.utils.CStructReader(d['head'])
+         ctl  = fm.utils.CStructReader(d['ctl'])
 
-    prog = ut.inprogress('reading the backendlog', 50)
+    with open('struct_mac.yaml') as f:
+         d = yaml.load(f)
+         obs = fm.utils.CStructReader(d['obs'])
+         dat = fm.utils.CStructReader(d['dat'])
+
+    prog = fm.utils.inprogress('reading backendlog', 100)
 
     def EOF(f):
         prog.next()
-        headinfo = ut.readbinary(f, HEAD)
-        return (headinfo['crec_type'] == 'ED')
+        head.read(f)
+        flag = head._data['crec_type'][-1][0]
+        return (flag == 'ED')
 
     # read data
-    data = OrderedDict()
-
     with open(backendlog, 'rb') as f:
         ## control info
-        ut.readbinary(f, HEAD)
-        ctlinfo = ut.readbinary(f, CTL)
+        EOF(f)
+        ctl.read(f)
 
         ## observation info
-        ut.readbinary(f, HEAD)
-        obsinfo = ut.readbinary(f, OBS)
+        EOF(f)
+        obs.read(f)
 
         ## data info
-        for key in DAT:
-            if not re.search('dmy', key):
-                data[key] = []
-
         while not EOF(f):
-            datinfo = ut.readbinary(f, DAT)
-            for key in data:
-                data[key].append(datinfo[key])
+            dat.read(f)
 
     # edit data
-    for key in data:
-        data[key] = np.asarray(data[key])
-
+    data = dat.data
     data['STARTTIME'] = data.pop('cint_sttm')
     data['ARRAYID']   = data.pop('cary_name')
     data['SCANTYPE']  = data.pop('cscan_type')
     data['ARRAYDATA'] = data.pop('iary_data')
 
-    # starttime
-    c = ut.DatetimeConverter('%Y%m%d%H%M%S.%f')
+    ## starttime
+    c = fm.utils.DatetimeConverter('%Y%m%d%H%M%S.%f')
     data['STARTTIME'] = np.array([c(t[:-2]) for t in data['STARTTIME']])
 
-    # arraydata
-    ons  = ut.where(data['SCANTYPE'] == 'ON')
-    rs   = ut.where(data['SCANTYPE'] == 'R')
-    skys = ut.where(data['SCANTYPE'] == 'SKY')
+    ## arraydata
+    ons  = fm.utils.where(data['SCANTYPE'] == 'ON')
+    rs   = fm.utils.where(data['SCANTYPE'] == 'R')
+    skys = fm.utils.where(data['SCANTYPE'] == 'SKY')
 
     ## apply scaling factor and offset
     arraydata = data['ARRAYDATA'].astype(float)
@@ -277,15 +278,12 @@ def _read_backendlog_mac(backendlog):
 
     data['ARRAYDATA'] = arraydata
 
-    # read formats
-    fmts = ut.getfitsformat(DAT)
-
-    # edit formats
+    # read and edit formats
+    fmts = dat.fitsformats
     fmts['STARTTIME'] = fmts.pop('cint_sttm')
     fmts['ARRAYID']   = fmts.pop('cary_name')
     fmts['SCANTYPE']  = fmts.pop('cscan_type')
     fmts['ARRAYDATA'] = fmts.pop('iary_data')
-
     fmts['STARTTIME'] = 'A26' # YYYY-mm-ddTHH:MM:SS.ssssss
     fmts['ARRAYDATA'] = '{[0]}E'.format(re.findall('\d+', fmts['ARRAYDATA']))
 
@@ -293,8 +291,8 @@ def _read_backendlog_mac(backendlog):
     header = fits.Header()
     header['EXTNAME'] = 'BACKEND'
     header['FILENAME'] = backendlog
-    header['CTLINFO'] = json.dumps(ctlinfo)
-    header['OBSINFO'] = json.dumps(obsinfo)
+    header['CTLINFO'] = ctl.jsondata
+    header['OBSINFO'] = obs.jsondata
 
     columns = []
     for key in data:
@@ -321,10 +319,9 @@ def _make_obsinfo(fitsobj):
     N = obsinfo['iary_num']
     flag = np.array(obsinfo['iary_usefg'], dtype=bool)
 
-    fitstype = 'FMFITS{}'.format(__version__)
-    frontend = np.unique(np.array(obsinfo['cfe_type'])[flag])[0]
-    backend  = ctlinfo['cbe_type']
-
+    fitstype  = 'FMFITS{}'.format(fm.__version__)
+    frontend  = np.unique(np.array(obsinfo['cfe_type'])[flag])[0]
+    backend   = ctlinfo['cbe_type']
     arrayid   = np.unique(datinfo['ARRAYID'])
     sideband  = np.array(obsinfo['csid_type'])[flag]
     interval  = np.tile(obsinfo['diptim'], N)
