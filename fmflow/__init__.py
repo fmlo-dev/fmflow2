bfec4e2f96e46d4e5f7f211d6ef23a4de82984dd
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-02-14 19:18:50 +0900

    Update package for v0.2

--- fmflow/__init__.py
+++ fmflow/__init__.py
@@ -1,11 +1,15 @@
 # coding: utf-8
 
 # Python 3.x compatibility
-from __future__ import absolute_import as __absolute_import
-from __future__ import division as __division
-from __future__ import print_function as __print_function
+from __future__ import absolute_import as _absolute_import
+from __future__ import division as _division
+from __future__ import print_function as _print_function
 
 # FMFlow information
-__version__ = 'v0.1'
+__version__ = 'v0.2'
 __author__  = 'Akio Taniguchi'
 
+# FMFlow submodules
+from .core import *
+from . import io
+from . import model
