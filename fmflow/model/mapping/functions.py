f939ad352a10da99be703040a26bea163545ee56
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-02-16 21:26:05 +0900

    Add functions of mapping

---
+++ fmflow/model/mapping/functions.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+# Python 3.x compatibility
+from __future__ import absolute_import as _absolute_import
+from __future__ import division as _division
+from __future__ import print_function as _print_function
+
+# the Python Package Index
+import numpy as np
+import fmflow as fm
+from astropy import units as u
+
+# imported items
+__all__ = []
