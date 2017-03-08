a1e6a3222f59cdea9e8cf4e7adcf86cdb2b0cdac
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-03-09 00:23:03 +0900

    Rename decomposition --> commonmode

--- fmflow/model/__init__.py
+++ fmflow/model/__init__.py
@@ -6,20 +6,20 @@ from __future__ import division as _division
 from __future__ import print_function as _print_function
 
 # FMFlow submodules
-from .decomposition import *
+from .commonmode import *
 from .mapping import *
 from .spectrum import *
 from .testing import *
 
 # imported items
 __all__ = []
-__all__ += decomposition.__all__
+__all__ += commonmode.__all__
 __all__ += mapping.__all__
 __all__ += spectrum.__all__
 __all__ += testing.__all__
 
 # delete submodules
-del decomposition
+del commonmode
 del mapping
 del spectrum
 del testing
